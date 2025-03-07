21ee22ff710d45ba4785ceffd9e31dead87ed1e8
Kouhakouu <phong.bq224887@sis.hust.edu.vn>
2025-03-07 07:45:48 +0700

    831

---
+++ src/app/forGiang/page.tsx
@@ -0,0 +1,12 @@
+import HeartAnimation from "@/components/forGiang"
+import ForGiangComponent from "@/components/forGiang"
+
+
+const forGiang = () => {
+    return (
+        <HeartAnimation />
+
+    )
+}
+
+export default forGiang
