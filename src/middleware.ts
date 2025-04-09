92c8440f45c845ef9a7452f1e95297b1b3c241b6
Kouhakouu <phong.bq224887@sis.hust.edu.vn>
2025-04-09 10:16:35 +0700

    up protect

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
     const token = request.cookies.get("token")?.value; // Lấy token từ cookies
 
     // Nếu không có token và truy cập /dashboard hoặc /assistant, chuyển hướng về /auth/login
-    if (!token && (request.nextUrl.pathname.startsWith("/dashboard") || request.nextUrl.pathname.startsWith("/assistant"))) {
+    if (!token && (request.nextUrl.pathname.startsWith("/dashboard") || request.nextUrl.pathname.startsWith("/assistant") || request.nextUrl.pathname.startsWith("/teacher") || request.nextUrl.pathname.startsWith("/manager"))) {
         return NextResponse.redirect(new URL("/auth/login", request.url));
     }
 
@@ -15,5 +15,5 @@ export function middleware(request: NextRequest) {
 
 // Áp dụng middleware cho cả /dashboard/* và /assistant/*
 export const config = {
-    matcher: [],
+    matcher: ["/dashboard/:path*", "/assistant/:path*", "/teacher/:path*", "/manager/:path*"],
 };
